88e819dfc811d436240101c216d91c9c6ad165d7
johnny zhang <johnnyzhang@umail.ucsb.edu>
2018-01-22 08:56:03 -0800

    created basic test file

--- quick_reaction_game.py
+++ quick_reaction_game.py
@@ -11,7 +11,8 @@ class player:
 	
 	def __init__(self, name, io_button):
 		self.playername = name
-		self.io_button = io_button
+		self.io_button = Button(io_button)
+
 
 	def getName(self):
 		return self.playername
@@ -22,19 +23,38 @@ class player:
 
 class board:
 
-	def __init__(self):
+	def __init__(self, p1, p2):
 		self.button1 = Button(io_button1)
 		self.button2 = Button(io_button2)
 		self.light = LED(io_led)
+		self.is_pressed = False
+		self.player1 = p1
+		self.player2 = p2
 
 	def pressed(player):
 		if player.getButton() == 14:
 			print(player.getName())
 		else:
 			print(player.getName())
+		is_pressed = True
+		light.off()
 
 	def start(self):
 		sleep(uniform(5,10))
 		light.on()
-		
+		button1.when_pressed = pressed(player1)
+		button2.when_pressed = pressed(player2)
 
+
+if __name__ == '__main__':
+	main()
+
+def main():
+	b = board()
+	player1 = player(input("Enter player 1 name here"), io_button1)
+	player2 = player(input("Enter player 2 name here"), io_button2)
+
+	b.start()
+
+
+	exit(0)
---
+++ quick_reaction_test.py
@@ -0,0 +1,15 @@
+import unittest
+import quick_reaction_game
+
+class PersonTest(unittest.TestCase):
+    def setUp(self):
+    	self.person = Person("test1",10)
+
+    def testName(self):
+    	self.assertEqual(Person.getName(), "test1")
+
+    def testIOport(self):
+    	self.assertEqual(Person.getButton(), 10)
+
+    def tearDown(self):
+    	self.person.dispose()
