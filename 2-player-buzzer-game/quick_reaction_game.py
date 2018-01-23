26677917d99be00d7a8983e64abbde1b8bdc58a9
johnnyzhang-raspberrypi <johnny.zhang916@gmail.com>
2018-01-23 20:15:52 +0000

    game is working, needs to implement player names and more unit testing

--- 2-player-buzzer-game/quick_reaction_game.py
+++ 2-player-buzzer-game/quick_reaction_game.py
@@ -28,22 +28,30 @@ class board:
 		self.is_pressed = False
 		self.player1_button = Button(button1)
 		self.player2_button = Button(button2)
+		self.score_player1 = 0
+		self.score_player2 = 0
 
-	def pressed(self, player):
-		print(player.getName())
-		self.is_pressed = True
+	def pressed(self, button):
+		if button.pin.number == io_button1:
+			self.score_player1 += 1
+		else:
+			self.score_player2 += 1
 		self.light.off()
+		print("Score is: Player1 @ "+str(self.score_player1)+" vs. Player2 @ "+str(self.score_player2))
 
 	def start(self, p1, p2):
-		sleep(uniform(5,10))
-		self.light.on()
-		self.player1_button.when_pressed = self.pressed(p1)
-		self.player2_button.when_pressed = self.pressed(p2)
+
+		while max(self.score_player2,self.score_player1) < 3:
+			sleep(uniform(4,7))
+			self.light.on()
+			self.player1_button.when_pressed = self.pressed
+			self.player2_button.when_pressed = self.pressed
+		print("game over")
 
 def main():
 	
-	player1 = player("J", io_button1)
-	player2 = player("Z ", io_button2)
+	player1 = player(("Enter player 1 name "), io_button1)
+	player2 = player(("Enter player 2 name "), io_button2)
 	b = board(io_button1, io_button2)
 	b.start(player1, player2)
 
