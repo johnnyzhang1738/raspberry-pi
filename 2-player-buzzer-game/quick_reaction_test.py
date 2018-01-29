a326c5e4907884ba577bb87f15a979db0204a09e
johnnyzhang-raspberrypi <johnny.zhang916@gmail.com>
2018-01-29 20:31:29 +0000

    got some tests working

--- 2-player-buzzer-game/quick_reaction_test.py
+++ 2-player-buzzer-game/quick_reaction_test.py
@@ -2,27 +2,41 @@ import unittest
 from quick_reaction_game import Player, Board
 
 class PersonTest(unittest.TestCase, Player):
-    @classmethod
-    def setUpClass(cls):
-        cls.Player.__init__("test1",10)
-    
-    def test_name(self):
-    	self.assertEqual(self.Player.getName(), "test1")
+	
+	def setUp(self):
+		Player.__init__(self,"test1",100)
+	
+	def testName(self):
+		self.assertEqual(Player.getName(self), "test1")
 
-    @unittest.expectedFailure
-    def test_IO_port_failure(self):
-    	self.assertEqual(person.getID(), 1, "broken")
+	@unittest.expectedFailure
+	def test_IO_port_failure(self):
+		self.assertEqual(Player.getID(self), 1, "broken")
 
-    @classmethod
-    def tearDownClass(cls):
-    	cls.person.dispose()
-
+	def test_ID(self):
+		self.assertEqual(Player.getID(self), 100)
+	
+	def tearDown(self):
+		del self
+		
 def suite_person():
-    suite = unittest.TestSuite()
-    suite.addTest(PersonTest('test_name'))
-    #suite.addTest(PersonTest('test_IO_port_failure'))
+	suite = unittest.TestSuite()
+	suite.addTest(PersonTest('testName'))
+	suite.addTest(PersonTest('test_IO_port_failure'))
+	suite.addTest(PersonTest('test_ID'))
 
-    return suite
+	return suite
+
+class BoardTest(unittest.TestCase, Board):
+	def setUp(self):
+		Board.__init__(self, 20, 21)
+		
+	def test_button_press(self):
+		pass
+	
+	def tearDown(self):
+		del self
+
 
 if __name__ == '__main__':
     runner = unittest.TextTestRunner()
