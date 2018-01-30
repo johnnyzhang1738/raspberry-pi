30181446a6410e9abd3f7fe44ee75f4228af217f
johnny zhang <johnnyzhang@umail.ucsb.edu>
2018-01-29 22:12:01 -0800

    created board tests

--- 2-player-buzzer-game/quick_reaction_test.py
+++ 2-player-buzzer-game/quick_reaction_test.py
@@ -1,5 +1,6 @@
 import unittest
 from quick_reaction_game import Player, Board
+from gpiozero import LED
 
 class PersonTest(unittest.TestCase, Player):
 	
@@ -30,14 +31,35 @@ def suite_person():
 class BoardTest(unittest.TestCase, Board):
 	def setUp(self):
 		Board.__init__(self, 20, 21)
+		p1 = Player("test_name1",20)
+		p2 = Player("test_name2", 21)
 		
-	def test_button_press(self):
-		pass
+	def test_init_buttons(self):
+		self.assertEqual(Board.player1_button,20)
+		self.assertEqual(Board.plyer2_button,21)
+		self.assertEqual(Board.is_pressed, False)
+		self.assertIs(Board.light, LED)
+
+	def test_init_players(self):
+		self.assertEqual(Board.score_player1, 0)
+		self.assertEqual(Board.score_player2, 0)
+
+	def test_pressed_func(self):
+		Board.pressed(20)
+		self.assertEqual(Board.score_player1,1)
 	
 	def tearDown(self):
 		del self
 
+def suite_board():
+	suite = unittest.TestSuite()
+	suite.addTest(BoardTest('test_init_buttons'))
+	suite.addTest(BoardTest('test_init_players'))
+	suite.addTest(BoardTest('test_pressed_func'))
+
+	return suite
 
 if __name__ == '__main__':
     runner = unittest.TextTestRunner()
     runner.run(suite_person())
+    runner.run(suite_board())
