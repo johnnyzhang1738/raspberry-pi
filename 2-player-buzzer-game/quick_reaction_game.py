5611702771fab3490ffe2325260685d5ab37b598
johnnyzhang-raspberrypi <johnny.zhang916@gmail.com>
2018-01-30 20:07:25 +0000

    fixed bugs

--- 2-player-buzzer-game/quick_reaction_game.py
+++ 2-player-buzzer-game/quick_reaction_game.py
@@ -5,7 +5,8 @@ from random import uniform
 io_button1 = 14
 io_button2 = 15
 io_led = 4
-
+p1_name = input("Enter Player 1 Name Here: ")
+p2_name = input("Enter Player 2 Name Here: ")
 
 class Player:
 	
@@ -32,16 +33,22 @@ class Board:
 		self.score_player2 = 0
 
 	def pressed(self, button):
-		if button.pin.number == io_button1:
+		
+		if (self.is_pressed==True):
+				return
+		if button.pin.number == io_button2:
 			self.score_player1 += 1
 		else:
 			self.score_player2 += 1
 		self.light.off()
-		print("Score is: Player1 @ "+str(self.score_player1)+" vs. Player2 @ "+str(self.score_player2))
-
+		
+		print("Score is: "+ str(p1_name)+ " @ "+str(self.score_player1)+" vs. "+str(p2_name)+" @ "+str(self.score_player2))
+		self.is_pressed = True
+				
 	def start(self, p1, p2):
-
+        
 		while max(self.score_player2,self.score_player1) < 3:
+			self.is_pressed = False
 			sleep(uniform(4,7))
 			self.light.on()
 			self.player1_button.when_pressed = self.pressed
@@ -49,9 +56,9 @@ class Board:
 		print("game over")
 
 def main():
-	
-	player1 = Player(input("Enter Player 1 Name Here: "), io_button1)
-	player2 = Player(input("Enter Player 2 Name Here: "), io_button2)
+
+	player1 = Player(p1_name, io_button1)
+	player2 = Player(p2_name, io_button2)
 	b = Board(io_button1, io_button2)
 	b.start(player1, player2)
 
